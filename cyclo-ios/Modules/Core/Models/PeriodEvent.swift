fb96f886f507171cde3f2a60d18d475f1e16b3e4
Rohit Suresh <rohit.suresh1993@gmail.com>
2025-09-08 14:27:50 -0700

    modified models

--- cyclo-ios/Modules/Core/Models/PeriodEvent.swift
+++ cyclo-ios/Modules/Core/Models/PeriodEvent.swift
@@ -9,13 +9,37 @@ import Foundation
 import SwiftData
 
 @Model
-class PeriodLog: Codable, Identifiable {
+class PeriodLog {
     var user: User
     var startDate: Date
     var endDate: Date
     
-    init(startDate, endDate) {
+    init(user: User, startDate: Date, endDate: Date) {
+        self.user = user
         self.startDate = startDate
         self.endDate = endDate
+    }
+}
+
+struct PeriodLogDTO: Codable, Identifiable {
+    var id: UUID = UUID()   // stable identity for SwiftUI Lists if needed
+    var startDate: Date
+    var endDate: Date
+    var userId: UUID        // send a reference to the user instead of embedding User
+}
+
+extension PeriodLog {
+    func toDTO() -> PeriodLogDTO {
+        PeriodLogDTO(
+            startDate: self.startDate,
+            endDate: self.endDate,
+            userId: self.user.uuid  // assuming User also has a UUID
+        )
+    }
+}
+
+extension PeriodLogDTO {
+    func toModel(user: User) -> PeriodLog {
+        PeriodLog(user: user, startDate: self.startDate, endDate: self.endDate)
     }
 }
--- cyclo-ios/Modules/Core/Models/User.swift
+++ cyclo-ios/Modules/Core/Models/User.swift
@@ -10,10 +10,13 @@ import SwiftData
 
 @Model
 class User: Identifiable {
+    @Attribute(.unique) var uuid: UUID
     var name: String
     var email: String
+    var dateOfBirth: Date
     
     init(name: String, email: String, dateOfBirth: Date) {
+        self.uuid = UUID()
         self.name = name
         self.email = email
         self.dateOfBirth = dateOfBirth
